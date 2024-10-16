a9d9739476137cef859b8063e8a03a12716f36f8
wintangz <wintang2003@icloud.com>
2024-10-17 00:34:10 +0700

    add pagination for shops

--- app/_services/shopService.ts
+++ app/_services/shopService.ts
@@ -3,4 +3,4 @@ import { axiosGet, axiosPost } from './baseService';
 const baseUrl = process.env.NEXT_PUBLIC_USER_HOST;
 
 export const getShops = (page: number) =>
-  axiosGet(baseUrl + '/profiles?size=10&page=' + page, '');
+  axiosGet(baseUrl + '/profiles?size=5&page=' + page, '');
