ea50b394dad29a0c04aa367a1ee596158135ac6f
wintangz <wintang2003@icloud.com>
2024-10-17 03:24:52 +0700

    add suspense

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import '../globals.css';
 import NavBar from '../_components/header/NavBar';
 import Footer from '../_components/footer/Footer';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Lovely Glam',
@@ -17,7 +18,9 @@ export default function RootLayout({
     <html lang='en'>
       <body>
         <NavBar />
-        <div className='min-h-screen bg-gray-100'>{children}</div>
+        <div className='min-h-screen bg-gray-100'>
+          <Suspense>{children}</Suspense>
+        </div>
         <Footer />
       </body>
     </html>
