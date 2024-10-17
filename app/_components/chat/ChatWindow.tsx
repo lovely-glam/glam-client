102ddff9d7df53953b938c7b5fd0314c27b1cfbf
AnataAria <kakata28419@gmail.com>
2024-10-17 23:48:59 +0700

    Fix chat windows

--- app/_components/chat/ChatWindow.tsx
+++ app/_components/chat/ChatWindow.tsx
@@ -2,7 +2,7 @@
 "use client";
 import { CompatClient, Stomp } from "@stomp/stompjs";
 import React, { useEffect, useState } from "react";
-import {getRoomsByShopId, ChatRoom} from '../../_services/chatService'
+import { getRoomsByShopId, ChatRoom } from '../../_services/chatService'
 enum EChatMessageStatus {
   JOIN,
   CHATTING,
@@ -38,9 +38,10 @@ const ChatWindow: React.FC<{ shopProfileId: number }> = ({ shopProfileId }) => {
   const [messages, setMessages] = useState<ChatMessageResponse[]>([]);
   const [newMessage, setNewMessage] = useState("");
   const [chatSocket, setChatSocket] = useState<CompatClient | null>(null);
-  const [isOpen, setIsOpen] = useState<boolean> (true);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
   const [room, setRoom] = useState<ChatRoom>();
-  
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
 
   const connectSocket = async () => {
     const socket = new WebSocket(webSocketUrl());
@@ -57,13 +58,13 @@ const ChatWindow: React.FC<{ shopProfileId: number }> = ({ shopProfileId }) => {
             if (!existingIds.has(msg.id)) {
               return [
                 ...prevMessages,
-                { ...msg, time: new Date().toLocaleTimeString(),  },
+                { ...msg, time: new Date().toLocaleTimeString(), },
               ]
             }
             return prevMessages;
           });
         }, {
-          Authorization: `Bearer ${localStorage.getItem("accessToken")}` 
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`
         });
 
         client.send(`/app/chat-histories/${room?.id}`, { Authorization: `Bearer ${localStorage.getItem("accessToken")}` });
@@ -74,26 +75,31 @@ const ChatWindow: React.FC<{ shopProfileId: number }> = ({ shopProfileId }) => {
     );
   };
   useEffect(() => {
-    const fetchRoomInformation = async (): Promise<ChatRoom | undefined> => {
-      const roomInfo: ChatRoom | undefined = await getRoomsByShopId(shopProfileId).then((response) => {
-        if (response.status === 200) {
+    if (!localStorage?.getItem("accessToken")) {
+      setIsOpen(false);
+      setIsAuthenticated(false);
+    } else {
+      const fetchRoomInformation = async (): Promise<ChatRoom | undefined> => {
+        const roomInfo: ChatRoom | undefined = await getRoomsByShopId(shopProfileId).then((response) => {
+          if (response.status === 200) {
             return response.data.content;
+          }
+          return undefined;
+        }).catch(error => {
+          return undefined;
+        });
+        return roomInfo;
+      };
+      const handleUp = async () => {
+        const roomInfo = await fetchRoomInformation();
+        console.log(roomInfo);
+        if (roomInfo) {
+          setRoom(roomInfo);
         }
-        return undefined;
-      }).catch(error => {
-        return undefined;
-      });
-      return roomInfo;
-    };
-    const handleUp = async () => {
-      const roomInfo = await fetchRoomInformation();
-      console.log(roomInfo);
-      if (roomInfo) {
-        setRoom(roomInfo);
       }
+      handleUp();
     }
 
-    handleUp();
     return () => {
     };
   }, [shopProfileId]);
@@ -124,21 +130,26 @@ const ChatWindow: React.FC<{ shopProfileId: number }> = ({ shopProfileId }) => {
     }
   };
 
+  const toggleChatWindow = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <>
-      {isOpen && (
+      {isOpen ? (
         <div className="fixed bottom-5 right-5 w-72 h-[500px] rounded-lg shadow-lg flex flex-col bg-white">
           <div className="flex items-center bg-red-400 text-white p-4 rounded-t-lg">
             <img src="/icon.ico" alt="Lovely Glam Icon" className="w-8 h-8 mr-3" />
             <span>Lovely Glam</span>
+            <button onClick={toggleChatWindow} className="ml-auto text-white">
+              <img src="https://www.svgrepo.com/show/275233/x.svg" alt="Close" className="w-6 h-6" />
+            </button>
           </div>
           <div className="flex-1 overflow-y-auto p-4">
             {messages.map((msg, index) => (
               <div
                 key={index}
-                className={`my-2 p-3 rounded-lg max-w-[66%] ${
-                  msg.from === "bot" ? "bg-gray-200 self-start" : "bg-green-200 self-end ml-auto"
-                }`}
+                className={`my-2 p-3 rounded-lg max-w-[66%] ${msg.from === "bot" ? "bg-gray-200 self-start" : "bg-green-200 self-end ml-auto"
+                  }`}
                 style={{
                   alignSelf: msg.from === "ROLE_USER" ? "flex-start" : "flex-end",
                   width: "fit-content",
@@ -158,10 +169,17 @@ const ChatWindow: React.FC<{ shopProfileId: number }> = ({ shopProfileId }) => {
               className="flex-1 p-2 border border-gray-300 rounded-md focus:outline-none"
             />
             <button onClick={sendMessage} className="ml-2">
-              <img src="https://www.svgrepo.com/show/30442/send.svg" alt="Send" className="w-6 h-6" />
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 32 32">
+                <path d="M 16 3 C 8.832031 3 3 8.832031 3 16 C 3 23.167969 8.832031 29 16 29 C 23.167969 29 29 23.167969 29 16 C 29 8.832031 23.167969 3 16 3 Z M 16 5 C 22.085938 5 27 9.914063 27 16 C 27 22.085938 22.085938 27 16 27 C 9.914063 27 5 22.085938 5 16 C 5 9.914063 9.914063 5 16 5 Z M 12.21875 10.78125 L 10.78125 12.21875 L 14.5625 16 L 10.78125 19.78125 L 12.21875 21.21875 L 16 17.4375 L 19.78125 21.21875 L 21.21875 19.78125 L 17.4375 16 L 21.21875 12.21875 L 19.78125 10.78125 L 16 14.5625 Z"></path>
+              </svg>
             </button>
           </div>
         </div>
+      ) : (isAuthenticated && (
+        <button onClick={toggleChatWindow} className="fixed bottom-5 right-5 w-14 h-14 rounded-full bg-red-400 flex items-center justify-center shadow-lg">
+          <img src="/icon.ico" alt="Chat" className="w-8 h-8" />
+        </button>
+      )
       )}
     </>
   );
