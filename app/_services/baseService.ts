3430e668ae1ba916a2674572af036adfa7c7dc3c
AnataAria <kakata28419@gmail.com>
2024-10-18 22:38:47 +0700

    Add new service

--- app/_services/baseService.ts
+++ app/_services/baseService.ts
@@ -4,7 +4,7 @@ export interface IResponseObject<T> {
   content: T;
   code: string;
   message: string;
-  isSuccess: boolean;
+  success: boolean;
   requestTime: Date;
   status: string;
 }
@@ -13,5 +13,11 @@ export const axiosGet = (url: string, config: any) => axios.get(url, config);
 export const axiosPost = (url: string, data: any, config: any) =>
   axios.post(url, data, config);
 
+export const axiosPut = (url: string, data: any, config: any) =>
+  axios.put(url, data, config);
+
 export const axiosDelete = (url: string, config: any) =>
   axios.delete(url, config);
+
+export const axiosPatch = (url: string, data: any, config: any) =>
+  axios.put(url, data, config);
