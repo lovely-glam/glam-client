1fc9fec3a7bebf5bb4f54cb0723ca2490ca66b88
AnataAria <kakata28419@gmail.com>
2024-10-19 00:26:15 +0700

    modify business service

--- app/_services/businessService.ts
+++ app/_services/businessService.ts
@@ -1,8 +1,15 @@
-import { axiosGet } from './baseService';
+import { AxiosResponse } from "axios";
+import { axiosGet, axiosPost, axiosPut, IResponseObject } from "./baseService";
+import { NailProfileDetailResponse } from "@/app/(admin)/(business)/business/profile/page"
+import { ServiceModel } from "../_components/business/ServiceModal";
 
 const baseUrl = process.env.NEXT_PUBLIC_BUSINESS_HOST;
 
-export const getCurrentShop = (token: string) =>
-  axiosGet(baseUrl + '/profiles/me', {
-    headers: { Authorization: 'Bearer ' + token },
-  });
+var token: string | null = '';
+
+if (typeof window !== 'undefined') {
+  token = 'Bearer ' + localStorage?.getItem('accessToken');
+}
+export const getProfiles = ():Promise<AxiosResponse<IResponseObject<NailProfileDetailResponse>>> => axiosGet(baseUrl + '/profiles/me', {headers: {Authorization: token}});
+
+export const updateProfileService = (data: ServiceModel): Promise<AxiosResponse<IResponseObject<NailProfileDetailResponse>>> => axiosPut(baseUrl + `/nail-services/${data.id}`, (({id, ...restData }) => restData)(data), {headers: {Authorization: token}});
