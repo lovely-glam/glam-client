7ffd210243a2caab691066c9ccc490de719ae8b2
AnataAria <kakata28419@gmail.com>
2024-10-21 22:44:43 +0700

    Add search

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,10 +1,23 @@
+"use client"
 import Image from 'next/image';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import ShopCard from '../_components/home/ShopCard';
 import Review from '../_components/home/Review';
 import Website from '../_components/home/Website';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const router = useRouter();
+  const handleSearch = () => {
+    if (searchQuery.trim()) {
+      const query = { address: searchQuery.trim() };
+
+      const params = new URLSearchParams(query).toString();
+      router.push(`/shops?q=${params}`);
+  }
+  };
   return (
     <div className='min-h-screen'>
       <section className='relative bg-pink-100 py-16 h-[35rem] flex items-center justify-center'>
@@ -20,13 +33,17 @@ export default function Home() {
         </div>
         <div className='text-center relative w-[33rem]'>
           <div className='relative min-w-lg w-full'>
-            <input
+          <input
               type='text'
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onSubmit={handleSearch}
               placeholder='Search for services or locations'
               className='input input-bordered w-full pl-10 rounded-full'
+              aria-label='Search for services or locations'
             />
             <FaMapMarkerAlt className='absolute top-1/2 transform -translate-y-1/2 left-3 text-gray-400' />
-            <button className='btn h-full absolute right-0 top-0 bg-black text-white rounded-full'>
+            <button className='btn h-full absolute right-0 top-0 bg-black text-white rounded-full' onClick={handleSearch}>
               <div className='p-1'>Search</div>
             </button>
           </div>
