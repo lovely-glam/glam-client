6550e14a2e2eeaec12bbcd8e3aca07a47b1801ea
AnataAria <kakata28419@gmail.com>
2024-10-24 05:55:07 +0700

    Add feedback form

--- app/_services/feedbackService.ts
+++ app/_services/feedbackService.ts
@@ -4,5 +4,13 @@ import { FeedbackResponse } from "../_components/home/Review";
 
 const baseUrl = process.env.NEXT_PUBLIC_USER_HOST;
 
-export const getShopFeedbackHome = (): Promise<AxiosResponse<IResponseObject<FeedbackResponse[]>>> => 
+var token: string | null = '';
+
+if (typeof window !== 'undefined') {
+    token = 'Bearer ' + localStorage?.getItem('accessToken');
+}
+
+export const getShopFeedbackHome = (): Promise<AxiosResponse<IResponseObject<FeedbackResponse[]>>> =>
     axiosGet(baseUrl + '/feedbacks/home', '');
+
+export const sendServiceFeedback = (data: { bookId: number, comment: string, vote: number }) => axiosPost(`${baseUrl}/feedbacks`, data, { headers: { Authorization: token } });
