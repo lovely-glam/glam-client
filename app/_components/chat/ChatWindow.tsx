8dff5c10b1fe737b374c84c4e239167ed229c82d
AnataAria <kakata28419@gmail.com>
2024-10-24 21:19:24 +0700

    Fix chat window bug display when close and fix bug icon display wrong on chat windows user

--- app/_components/chat/ChatWindow.tsx
+++ app/_components/chat/ChatWindow.tsx
@@ -3,6 +3,7 @@
 import { CompatClient, Stomp } from "@stomp/stompjs";
 import React, { useEffect, useState } from "react";
 import { getRoomsByShopId, ChatRoom } from '../../_services/chatService'
+import { FaPaperPlane, FaTimesCircle } from "react-icons/fa";
 enum EChatMessageStatus {
   JOIN,
   CHATTING,
@@ -39,7 +40,7 @@ const ChatWindow: React.FC<{ shopProfileId: number }> = ({ shopProfileId }) => {
   const [chatSocket, setChatSocket] = useState<CompatClient | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(true);
   const [room, setRoom] = useState<ChatRoom>();
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(true);
 
 
   const connectSocket = async () => {
@@ -138,7 +139,7 @@ const ChatWindow: React.FC<{ shopProfileId: number }> = ({ shopProfileId }) => {
             <img src="/icon.ico" alt="Lovely Glam Icon" className="w-8 h-8 mr-3" />
             <span>Lovely Glam</span>
             <button onClick={toggleChatWindow} className="ml-auto text-white">
-              <img src="https://www.svgrepo.com/show/275233/x.svg" alt="Close" className="w-6 h-6" />
+              <FaTimesCircle size={30}/>
             </button>
           </div>
           <div className="flex-1 overflow-y-auto p-4">
@@ -166,9 +167,7 @@ const ChatWindow: React.FC<{ shopProfileId: number }> = ({ shopProfileId }) => {
               className="flex-1 p-2 border border-gray-300 rounded-md focus:outline-none"
             />
             <button onClick={sendMessage} className="ml-2">
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 32 32">
-                <path d="M 16 3 C 8.832031 3 3 8.832031 3 16 C 3 23.167969 8.832031 29 16 29 C 23.167969 29 29 23.167969 29 16 C 29 8.832031 23.167969 3 16 3 Z M 16 5 C 22.085938 5 27 9.914063 27 16 C 27 22.085938 22.085938 27 16 27 C 9.914063 27 5 22.085938 5 16 C 5 9.914063 9.914063 5 16 5 Z M 12.21875 10.78125 L 10.78125 12.21875 L 14.5625 16 L 10.78125 19.78125 L 12.21875 21.21875 L 16 17.4375 L 19.78125 21.21875 L 21.21875 19.78125 L 17.4375 16 L 21.21875 12.21875 L 19.78125 10.78125 L 16 14.5625 Z"></path>
-              </svg>
+              <FaPaperPlane size={25}/>
             </button>
           </div>
         </div>
