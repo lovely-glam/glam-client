a7ac66c4ddff676c93f17a746c7a5a9f6844a316
AnataAria <kakata28419@gmail.com>
2024-11-08 00:11:41 +0700

    fix css on us page

--- app/(admin)/(admin)/admin/account/page.tsx
+++ app/(admin)/(admin)/admin/account/page.tsx
@@ -4,42 +4,42 @@ import UserAccountTable from '@/app/_components/admin/UserAccountTable';
 import { useState } from 'react';
 import { ToastContainer } from 'react-toastify'; // If you use toast notification
 const AccountManagement = () => {
-    
+
     const [activeTab, setActiveTab] = useState<'user' | 'business'>('business');
-    
+
     return (
         <main className="flex-1 p-6">
-                <ToastContainer />
+            <ToastContainer />
 
-                <h1 className="text-3xl font-bold text-gray-800 mb-6">Account Management</h1>
+            <h1 className="text-3xl font-bold text-gray-800 mb-6">Account Management</h1>
 
-                <div className="mb-4">
-                    <button
-                        className={`px-4 py-2 rounded-lg ${activeTab === 'business' ? 'bg-pink-600 text-white' : 'bg-white text-gray-800'} transition duration-200`}
-                        onClick={() => {
-                            setActiveTab('business');
-                        }}
-                    >
-                        Business Accounts
-                    </button>
-                    <button
-                        className={`ml-4 px-4 py-2 rounded-lg ${activeTab === 'user' ? 'bg-pink-600 text-white' : 'bg-white text-gray-800'} transition duration-200`}
-                        onClick={() => {
-                            setActiveTab('user');
-                        }}
-                    >
-                        User Accounts
-                    </button>
-                </div>
+            <div className="mb-4">
+                <button
+                    className={`px-4 py-2 rounded-lg ${activeTab === 'business' ? 'bg-pink-600 text-white' : 'bg-white text-gray-800'} transition duration-200`}
+                    onClick={() => {
+                        setActiveTab('business');
+                    }}
+                >
+                    Business Accounts
+                </button>
+                <button
+                    className={`ml-4 px-4 py-2 rounded-lg ${activeTab === 'user' ? 'bg-pink-600 text-white' : 'bg-white text-gray-800'} transition duration-200`}
+                    onClick={() => {
+                        setActiveTab('user');
+                    }}
+                >
+                    User Accounts
+                </button>
+            </div>
 
-                {activeTab === 'business' && (
-                    <BusinessAccountTable/>
-                )}
+            {activeTab === 'business' && (
+                <BusinessAccountTable />
+            )}
 
-                {activeTab === 'user' && (
-                    <UserAccountTable/>
-                )}
-            </main>
+            {activeTab === 'user' && (
+                <UserAccountTable />
+            )}
+        </main>
     );
 };
 
--- app/(app)/(public)/about/page.tsx
+++ app/(app)/(public)/about/page.tsx
@@ -1,33 +1,34 @@
+import Image from "next/image";
+
 const About = () => {
   return (
     <div className='flex flex-col md:flex-row p-8'>
       {/* Brand Section */}
       <div className='bg-secondary w-full md:w-1/2 flex flex-col items-center justify-center p-8 rounded-box shadow-lg'>
-        <div className='mb-6'>
-          <img src='path_to_your_image' alt='Lovely Glam' className='w-24' />
+        <div className='mb-6 w-full'>
+          <img src="/glam_poster.png" alt="" className="w-full h-auto object-cover" />
         </div>
-        <h1 className='text-4xl font-bold text-primary mb-2'>Lovely Glam</h1>
-        <p className='text-lg'>Effortless beauty, tailored to your schedule.</p>
       </div>
+
 
       {/* Story Section */}
       <div className='bg-white w-full md:w-1/2 p-8 rounded-box shadow-lg'>
         <div className='flex items-center justify-center'>
-          <h2 className='text-2xl font-bold mb-4 italic'>
+          <h2 className='text-3xl font-bold text-primary mb-4 italic'>
             Câu chuyện thương hiệu
           </h2>
         </div>
 
-        <h3 className='text-xl font-semibold text-primary mt-4'>Sứ mệnh:</h3>
+        <h3 className='text-xl font-bold text-primary mt-4'>SỨ MỆNH:</h3>
         <p className='text-base text-neutral mt-2'>
-          &quot;Tạo dựng trải nghiệm làm đẹp tiện lợi và thoải mái tối đa&quot;
+          <strong>&quot;Tạo dựng trải nghiệm làm đẹp tiện lợi và thoải mái tối đa&quot;</strong>
           <br />
           Nền tảng cung cấp hệ thống đặt lịch dễ sử dụng cho dịch vụ làm móng,
           nâng cao trải nghiệm khách hàng và hỗ trợ các salon trong quản lý lịch
           hẹn và phát triển kinh doanh.
         </p>
 
-        <h3 className='text-xl font-semibold text-primary mt-6'>Tầm nhìn:</h3>
+        <h3 className='text-xl font-bold text-primary mt-6'>TẦM NHÌN:</h3>
         <p className='text-base text-neutral mt-2'>
           Trở thành nền tẳng đặt lịch dịch vụ nail hàng đầu TP.HCM, mang lại sự
           tiện ích cho khách hàng và đối tác tin cậy cho các tiệm nail trong khu
@@ -36,8 +37,8 @@ const About = () => {
           nail chất lượng cao, mọi lúc, mọi nơi.
         </p>
 
-        <h3 className='text-xl font-semibold text-primary mt-6'>
-          Lovely Glam: Nâng tầm trải nghiệm làm nail của bạn
+        <h3 className='text-xl font-bold text-primary mt-6'>
+          LOVELY GLAM: NÂNG TẦM TRẢI NGHIỆM LÀM NAIL CỦA BẠN
         </h3>
         <p className='text-base text-neutral mt-2'>
           Lovely Glam ra đời với sứ mệnh kết nối khách hàng với những tiệm nail
@@ -48,20 +49,20 @@ const About = () => {
           <br />
           <br />
           Tại Lovely Glam, bạn sẽ được:
-          <br />• Tiết kiệm thời gian: Thay vì phải lân la tìm kiếm và gọi điện
+          <br />• <span className="font-bold">Tiết kiệm thời gian:</span> Thay vì phải lân la tìm kiếm và gọi điện
           thoại đặt lịch hẹn từng tiệm nail, bạn chỉ cần truy cập Lovely Glam,
           tìm kiếm tiệm nail theo nhu cầu và đặt lịch hẹn trực tuyến chỉ với vài
           thao tác đơn giản.
-          <br />• So sánh giá cả và dịch vụ: Lovely Glam cung cấp đầy đủ thông
+          <br />• <span className="font-bold">So sánh giá cả và dịch vụ:</span> Lovely Glam cung cấp đầy đủ thông
           tin về giá cả và dịch vụ của từng tiệm nail, giúp bạn dễ dàng so sánh
           và lựa chọn tiệm nail phù hợp nhất với nhu cầu của mình.
-          <br />• Đọc đánh giá của khách hàng: Lovely Glam hiển thị đánh giá của
+          <br />• <span className="font-bold">Đọc đánh giá của khách hàng:</span> Lovely Glam hiển thị đánh giá của
           khách hàng về từng tiệm nail, giúp bạn có thêm thông tin để đưa ra
           quyết định.
-          <br />• Đặt lịch hẹn nhanh chóng: Hệ thống đặt lịch hẹn trực tuyến của
+          <br />• <span className="font-bold">Đặt lịch hẹn nhanh chóng:</span> Hệ thống đặt lịch hẹn trực tuyến của
           Lovely Glam hoạt động 24/7, giúp bạn dễ dàng đặt lịch hẹn bất cứ lúc
           nào, bất cứ nơi đâu.
-          <br />• Nhận thông báo qua tin nhắn: Lovely Giam sẽ gửi thông báo qua
+          <br />• <span className="font-bold">Nhận thông báo qua tin nhắn:</span> Lovely Giam sẽ gửi thông báo qua
           tin nhắn để nhắc bạn về lịch hẹn sắp đến, giúp bạn không bỏ lỡ bất kỳ
           lịch hẹn nào.
           <br />
