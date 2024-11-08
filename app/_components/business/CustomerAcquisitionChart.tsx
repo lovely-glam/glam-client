732499dbbd1b8f6a63302f978c537c24b239eedb
korroo <anhvnvse172006@fpt.edu.vn>
2024-11-09 05:08:06 +0700

    dashboard business

---
+++ app/_components/business/CustomerAcquisitionChart.tsx
@@ -0,0 +1,63 @@
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const CustomerAcquisitionChart: React.FC = () => {
+  const data = {
+    labels: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
+    datasets: [
+      {
+        label: 'Users',
+        data: [200, 300, 250, 400, 350, 450, 500, 600, 550, 700, 650, 800],
+        borderColor: '#34b4eb',
+        backgroundColor: 'rgba(52, 180, 235, 0.2)',
+        fill: true,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+    },
+  };
+
+  return <Line data={data} options={options} />;
+};
+
+export default CustomerAcquisitionChart;
